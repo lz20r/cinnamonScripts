0cb452372d9ac14fe0f4a21f8134dfb5ce73068f
Nia <zhiyaonaiara@gmail.com>
2024-02-15 12:58:26 +0100

    Create start-client.sh

---
+++ start-client.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+clear
+cd /home/container
+
+clear                                 
+echo -e ".                \e[35m ⠀  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡄⠀⠀    \e[0m \e[31m                                                      \e[0m "
+echo -e ".                \e[35m   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠻⣷⠀⠀  \e[0m \e[31m                                                      \e[0m "
+echo -e ".                \e[35m  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⡀⠀⠀⢸⡿⣿⠇⠰⠟⠀⠀  \e[0m \e[31m     ____  _                                           \e[0m "
+echo -e ".                \e[35m   ⠀⠀⠀⠀⣀⡤⣤⠶⠛⠉⠉⠀⠀⠉⠉⠛⠲⣤⣤⣄⠀⠀⠀⠀⠀ \e[0m \e[31m   / ____(_)                                          \e[0m "
+echo -e ".                \e[35m   ⠀⠀⠀⡼⠃⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠈⢧⠀⠀⠀⠀  \e[0m \e[31m  | |     _ _ __  _ __   __ _ _ __ ___   ___  _ __    \e[0m "
+echo -e ".                \e[35m   ⠀⠀⡼⢁⡆⠀⢀⡀⠀⠀⠀ ⠀⠀⠀⠀⢀⡄⠀⢰⡈⢧     \e[0m \e[31m  | |    | | '_ \| '_ \ / _` | '_ ` _ \ / _ \| '_ \   \e[0m "
+echo -e ".                \e[35m   ⢀⡞⠁⣸⠁⠀⢠⠬⠓⠀⣀⠀⠀⢀⠀⠛⠥⣄⠀⠀⡇⠈⢳⡀ \e[0m \e[31m  | |____| | | | | | | | (_| | | | | | | (_) | | | |  \e[0m "
+echo -e ".                \e[35m   ⡞⠀⠀⠹⣆⠀⠀⠆⠀⠀⠓⠊⠙⠚⠁⠀⠀⠀⠀⣰⠇⠀⠀ ⢻ \e[0m \e[31m   \_____|_|_| |_|_| |_|\__,_|_| |_| |_|\___/|_| |_|  \e[0m "
+echo -e ".                \e[35m   ⣇⠀⠀⠀⠈⣹⠶⠦⠤⠤⣤⣤⣤⡤⠤⠤⠴⠶⣏⠁⠀⠀⠀⣸ \e[0m \e[31m                                                      \e[0m "
+echo -e ".                \e[35m   ⠈⠓⠲⠖⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⠲⠶⠚⠁  \e[0m \e[31m                                                      \e[0m "
+echo -e ".                \e[35m   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀⠀      \e[0m \e[31m                                                      \e[0m "
+echo " "
+echo " "
+echo -e "\e[33m - Cinammon Host™'s Discord Support Server en discord: https://discord.gg/3TUf9JBVbt \e[0m"
+echo -e "\e[33m - Momo™'s Discord Support Server: https://discord.gg/3TUf9JBVbt \e[0m"
+echo -e "\e[33m - Cinammon™'s Donations: https://www.patreon.com/cinammon \e[0m"
+echo -e "\e[33m - Cinammon™'s Donations: https://www.patreon.com/cinammon \e[0m"
+echo -e "\e[33m - Momo bot's Invitation: https://discord.gg/ezfkXgekw7"   \e[0m"
+echo " "
+echo ""
+echo -e ".\e[31m                                         © Cinammon.es 2024 \e[0m"
+echo " "
+echo " "
+sleep 3
+echo -e "\e[36m Le damos la bienvenida a su servidor! \e[0m"
+sleep 2
+echo -e "\e[36m Póngase cómodo mientras hacemos nuestra magia... \e[0m"
+sleep 2
+echo " "
+echo " "
+#echo -e "\e[33m Analizando contenedor... \e[0m"
+#sleep 5
+#echo -e "\e[32m ✔️ | No se encontraron scripts maliciosos. \e[0m"
+#echo " "
+#echo " "
+echo -e "\e[33m Cargando datos del servidor... \e[0m"
+sleep 2
+echo -e "\e[32m ✔️ | Plan actual: Netcat-Bot-JavaScript Free"
+echo -e "\e[32m ✔️ | Servicios extra: Ninguno"
+echo " "
+echo " "
+echo -e "\e[33m Cargando configuracion del startup... \e[0m"
+if [[ ! -z ${NODE_PACKAGES} ]]; then 
+    echo -e "\e[33m Instalando paquetes solicitados... \e[0m"
+    /usr/local/bin/npm install ${NODE_PACKAGES};
+else
+    echo -e "\e[32m ✔️ | No hay solicitudes de instalacion de paquetes. \e[0m"
+fi; 
+if [[ ! -z ${UNNODE_PACKAGES} ]]; then 
+    echo -e "\e[33m Desinstalando paquetes solicitados... \e[0m"
+    /usr/local/bin/npm uninstall ${UNNODE_PACKAGES};
+else
+    echo -e "\e[32m ✔️ | No hay solicitudes de desinstalacion de paquetes. \e[0m"
+fi; 
+echo " "
+echo " "
+echo -e "\e[33m Buscando el archivo de arranque en el /home/container... \e[0m"
+sleep 2
+
+if [[ -f  "${MAIN_FILE}" ]]; then
+    echo -e "\e[32m ✔️ | Detectado: /home/container/${MAIN_FILE} \e[0m"
+    sleep 2
+    echo -e "\e[33m Cargando /home/container/${MAIN_FILE}... \e[0m"
+    sleep 1
+    /usr/local/bin/node ${MAIN_FILE};
+    sleep 100
+    exit 0
+fi;
+if [ -f /home/container/index.js ]; then
+    echo -e "\e[32m ✔️ | Detectado: /home/container/index.js \e[0m"
+    sleep 2
+    echo -e "\e[33m Cargando /home/container/index.js... \e[0m"
+    sleep 1
+    /usr/local/bin/node /home/container/index.js
+    sleep 100
+    exit 0
+fi
+if [ -f /home/container/src/index.js ]; then
+    echo -e "\e[32m ✔️ | Detectado: /home/container/src/index.js \e[0m"
+    sleep 2
+    echo -e "\e[33m Cargando /home/container/src/index.js... \e[0m"
+    sleep 1
+    /usr/local/bin/node /home/container/src/index.js
+    sleep 100
+    exit 0
+fi
+    
+echo -e "\e[31m X | ERROR: No se ha encontrado ningun archivo principal de arranque. Deteniendo servidor... \e[0m"
+sleep 5
+exit 1
