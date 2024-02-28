4652f4c02533e3085804f9f503155f6dff160e38
Nia <zhiyaonaiara@gmail.com>
2024-02-28 09:08:20 +0100

    Update start-client.sh

--- start-client.sh
+++ start-client.sh
@@ -4,93 +4,33 @@ clear
 cd /home/container
 
 clear                                 
-echo -e ".                \e[35m     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⡄⠀⠀		\e[0m \e[31m																												    \e[0m "
-echo -e ".                \e[35m     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠻⣷⠀		\e[0m \e[31m																												    \e[0m "
-echo -e ".                \e[35m    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⡀⠀⠀⢸⡿⣿⠇⠰⠟⠀⠀	\e[0m \e[31m	   ▄▄█▀▀▀▄█  ██                                                              ▀██▀  ▀██▀                  ▄ 	    \e[0m "  
-echo -e ".                \e[35m     ⠀⠀⠀⠀⣀⡤⣤⠶⠛⠉⠉⠀⠀⠉⠉⠛⠲⣤⣤⣄⠀⠀⠀⠀⠀	\e[0m \e[31m	  ▄█▀     ▀  ▄▄▄  ▄▄ ▄▄▄    ▄▄▄▄   ▄▄ ▄▄ ▄▄   ▄▄ ▄▄ ▄▄     ▄▄▄   ▄▄ ▄▄▄       ██    ██    ▄▄▄    ▄▄▄▄  ▄██▄ 	\e[0m " 
-echo -e ".                \e[35m     ⠀⠀⠀⡼⠃⠈⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠁⠈⢧⠀⠀⠀⠀    \e[0m \e[31m     ██          ██  ██  ██  ▀▀ ▄██   ██ ██ ██   ██ ██ ██  ▄█  ▀█▄  ██  ██       ██▀▀▀▀██  ▄█  ▀█▄ ██▄ ▀   ██  	    \e[0m " 
-echo -e ".                \e[35m     ⠀⠀⡼⢁⡆⠀⢀⡀⠀⠀⠀ ⠀⠀⠀⠀⢀⡄⠀⢰⡈⢧⠀⠀⠀    \e[0m \e[31m     ▀█▄      ▄  ██  ██  ██  ▄█▀ ██   ██ ██ ██   ██ ██ ██  ██   ██  ██  ██       ██    ██  ██   ██ ▄ ▀█▄▄  ██   	\e[0m "
-echo -e ".                \e[35m     ⢀⡞⠁⣸⠁⠀⢠⠬⠓⠀⣀⠀⠀⢀⠀⠛⠥⣄⠀⠀⡇⠈⢳⡀⠀	 \e[0m \e[31m     ▀▀█▄▄▄▄▀  ▄██▄ ▄██▄ ██▄ ▀█▄▄▀█▀ ▄██ ██ ██▄ ▄██ ██ ██▄  ▀█▄▄█▀ ▄██▄ ██▄     ▄██▄  ▄██▄  ▀█▄▄█▀ █▀▄▄█▀  ▀█▄▀ 	\e[0m "
-echo -e ".                \e[35m     ⡞⠀⠀⠹⣆⠀⠀⠆⠀⠀⠓⠊⠙⠚⠁⠀⠀⠀⠀⣰⠇⠀⠀ ⢻⠀	 \e[0m \e[31m																												    \e[0m "
-echo -e ".                \e[35m     ⣇⠀⠀⠀⠈⣹⠶⠦⠤⠤⣤⣤⣤⡤⠤⠤⠴⠶⣏⠁⠀⠀⠀⣸⠀	 \e[0m \e[31m									   with love from supporter ❤												    \e[0m "
-echo -e ".                \e[35m     ⠈⠓⠲⠖⠚⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠓⠲⠶⠚⠁⠀	 \e[0m \e[31m																												    \e[0m "
-echo -e ".		          \e[35m                                         \e[0m \e[31m                  																								    \e[0m "
+echo -e "      :::::::: ::::::::::: ::::    ::: ::::    :::     :::       :::   :::    ::::::::  ::::    :::     :::::::::: :::::::: 
+    :+:    :+:    :+:     :+:+:   :+: :+:+:   :+:   :+: :+:    :+:+: :+:+:  :+:    :+: :+:+:   :+:     :+:       :+:    :+: 
+   +:+           +:+     :+:+:+  +:+ :+:+:+  +:+  +:+   +:+  +:+ +:+:+ +:+ +:+    +:+ :+:+:+  +:+     +:+       +:+         
+  +#+           +#+     +#+ +:+ +#+ +#+ +:+ +#+ +#++:++#++: +#+  +:+  +#+ +#+    +:+ +#+ +:+ +#+     +#++:++#  +#++:++#++   
+ +#+           +#+     +#+  +#+#+# +#+  +#+#+# +#+     +#+ +#+       +#+ +#+    +#+ +#+  +#+#+#     +#+              +#+    
+#+#    #+#    #+#     #+#   #+#+# #+#   #+#+# #+#     #+# #+#       #+# #+#    #+# #+#   #+#+# #+# #+#       #+#    #+#     
+######## ########### ###    #### ###    #### ###     ### ###       ###  ########  ###    #### ### ########## ########       "
 echo " "
 echo " "
 echo -e "\e[33m - Cinammon Host™'s Discord Support Server en discord: https://discord.gg/3TUf9JBVbt \e[0m"
 echo -e "\e[33m - Momo™'s Discord Support Server: https://discord.gg/3TUf9JBVbt \e[0m"
 echo -e "\e[33m - Cinammon™'s Donations: https://www.patreon.com/cinammon \e[0m"
 echo -e "\e[33m - Cinammon™'s Donations: https://www.patreon.com/cinammon \e[0m"
-echo -e "\e[33m - Momo bot's Invitation: https://discord.gg/ezfkXgekw7"   \e[0m"
+echo -e "\e[33m - Momo bot's Invitation: https://discord.gg/ezfkXgekw7   \e[0m"
 echo " "
 echo ""
 echo -e ".\e[31m                                         © Cinammon.es 2024 \e[0m"
 echo " "
 echo " "
+echo "Iniciando la plataforma de php para que ejecutarse sin errores..."
+/usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+
+echo "Inicializando Nginx, un segundo por favor."
 sleep 3
 echo -e "\e[36m Le damos la bienvenida a su servidor! \e[0m"
 sleep 2
-echo -e "\e[36m Póngase cómodo mientras hacemos nuestra magia... \e[0m"
-sleep 2
-echo " "
-echo " "
-#echo -e "\e[33m Analizando contenedor... \e[0m"
-#sleep 5
-#echo -e "\e[32m ✔️ | No se encontraron scripts maliciosos. \e[0m"
-#echo " "
-#echo " "
-echo -e "\e[33m Cargando datos del servidor... \e[0m"
-sleep 2
-echo -e "\e[32m ✔️ | Plan actual: Netcat-Bot-JavaScript Free"
-echo -e "\e[32m ✔️ | Servicios extra: Ninguno"
-echo " "
-echo " "
-echo -e "\e[33m Cargando configuracion del startup... \e[0m"
-if [[ ! -z ${NODE_PACKAGES} ]]; then 
-    echo -e "\e[33m Instalando paquetes solicitados... \e[0m"
-    /usr/local/bin/npm install ${NODE_PACKAGES};
-else
-    echo -e "\e[32m ✔️ | No hay solicitudes de instalacion de paquetes. \e[0m"
-fi; 
-if [[ ! -z ${UNNODE_PACKAGES} ]]; then 
-    echo -e "\e[33m Desinstalando paquetes solicitados... \e[0m"
-    /usr/local/bin/npm uninstall ${UNNODE_PACKAGES};
-else
-    echo -e "\e[32m ✔️ | No hay solicitudes de desinstalacion de paquetes. \e[0m"
-fi; 
-echo " "
-echo " "
-echo -e "\e[33m Buscando el archivo de arranque en el /home/container... \e[0m"
-sleep 2
-
-if [[ -f  "${MAIN_FILE}" ]]; then
-    echo -e "\e[32m ✔️ | Detectado: /home/container/${MAIN_FILE} \e[0m"
-    sleep 2
-    echo -e "\e[33m Cargando /home/container/${MAIN_FILE}... \e[0m"
-    sleep 1
-    /usr/local/bin/node ${MAIN_FILE};
-    sleep 100
-    exit 0
-fi;
-if [ -f /home/container/index.js ]; then
-    echo -e "\e[32m ✔️ | Detectado: /home/container/index.js \e[0m"
-    sleep 2
-    echo -e "\e[33m Cargando /home/container/index.js... \e[0m"
-    sleep 1
-    /usr/local/bin/node /home/container/index.js
-    sleep 100
-    exit 0
-fi
-if [ -f /home/container/src/index.js ]; then
-    echo -e "\e[32m ✔️ | Detectado: /home/container/src/index.js \e[0m"
-    sleep 2
-    echo -e "\e[33m Cargando /home/container/src/index.js... \e[0m"
-    sleep 1
-    /usr/local/bin/node /home/container/src/index.js
-    sleep 100
-    exit 0
-fi
-    
-echo -e "\e[31m X | ERROR: No se ha encontrado ningun archivo principal de arranque. Deteniendo servidor... \e[0m"
-sleep 5
-exit 1
+echo -e "\e[36m Póngase cómodo mientras hacemos nuestra magia...💻😜 \e[0m"
+echo -e "\e[36m Muchas gracias por elegirnos! su servidor esta preparado 😉\e[0m"
+/usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
+echo -e "\e[36m Muchas gracias por elegirnos! su servidor esta preparado\e[0m"
